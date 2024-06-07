e36a11823ce1d68fdc05ed289a4e9b9727f3f91f
StephenOren <orendain.stephen@gmail.com>
2024-06-06 18:01:55 -0700

    Added GPS functionality in Fragment

--- app/src/main/java/com/hfad/dev322projectdassh/TimerFragment.kt
+++ app/src/main/java/com/hfad/dev322projectdassh/TimerFragment.kt
@@ -1,10 +1,15 @@
 package com.hfad.dev322projectdassh
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.os.SystemClock
 import android.view.LayoutInflater
@@ -13,9 +18,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Chronometer
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
-class TimerFragment : Fragment(), SensorEventListener {
+class TimerFragment : Fragment(), SensorEventListener, LocationListener {
     private lateinit var stopwatch: Chronometer // The chronometer
     private var running = false // Is the chronometer running?
     private var offset: Long = 0 // The base offset for the chronometer
@@ -31,6 +39,13 @@ class TimerFragment : Fragment(), SensorEventListener {
     private var mAccelerometer : Sensor ?= null
     private var resume = false;
 
+    // GPS Stuff
+    private lateinit var locationManager: LocationManager
+    private lateinit var tvGpsLocation: TextView
+    private val locationPermissionCode = 2
+
+
+
     // needed for find view by id
     private var rootView: View? = null
 
@@ -42,6 +57,8 @@ class TimerFragment : Fragment(), SensorEventListener {
 
         rootView = view
 
+        // initialize
+        tvGpsLocation = view.findViewById(R.id.gpsTextView)
 
         // Initialize sensor manager and accelerometer
         mSensorManager = requireActivity().getSystemService(Context.SENSOR_SERVICE) as SensorManager
@@ -88,6 +105,12 @@ class TimerFragment : Fragment(), SensorEventListener {
         resetButton.setOnClickListener {
             offset = 0
             setBaseTime()
+        }
+
+        // Get location button
+        val getLocationButton = view.findViewById<Button>(R.id.getLocation)
+        getLocationButton.setOnClickListener {
+            getLocation()
         }
 
         return view
@@ -143,5 +166,45 @@ class TimerFragment : Fragment(), SensorEventListener {
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         return
+    }
+
+    //GPS Stuff
+    override fun onLocationChanged(location: Location) {
+        tvGpsLocation.text =
+            "Latitude: " + location.latitude + " , Longitude: " + location.longitude
+    }
+
+    //GPS Stuff
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == locationPermissionCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(requireContext(), "Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    //GPS Stuff
+    private fun getLocation() {
+        locationManager = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED)
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                locationPermissionCode
+            )
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
     }
 }
