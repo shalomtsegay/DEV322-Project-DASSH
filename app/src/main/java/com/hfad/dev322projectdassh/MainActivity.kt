b152f7947f1b0f6517d5e23267e7249f8be3f4fa
StephenOren <orendain.stephen@gmail.com>
2024-06-12 11:42:43 -0700

    Added bottom navigation

--- app/src/main/java/com/hfad/dev322projectdassh/MainActivity.kt
+++ app/src/main/java/com/hfad/dev322projectdassh/MainActivity.kt
@@ -2,94 +2,57 @@ package com.hfad.dev322projectdassh
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.Chronometer
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
-    /*    lateinit var stopwatch: Chronometer //The chronometer
-    var running = false //Is the chronometer running?
-    var offset: Long = 0 //The base offset for the chronometer
-
-    //Add key Strings for use with the Bundle
-    val OFFSET_KEY = "offset"
-    val RUNNING_KEY = "running"
-    val BASE_KEY = "base"*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /*//Get a reference to the stopwatch
-        stopwatch = findViewById<Chronometer>(R.id.stopwatch)
-        //Restore the previous state
-        if (savedInstanceState != null) {
-            offset = savedInstanceState.getLong(OFFSET_KEY)
-            running = savedInstanceState.getBoolean(RUNNING_KEY)
-            if (running) {
-                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
-                stopwatch.start()
-            } else setBaseTime()
-        }*/
 
-        /*        //start button
-        val startButton = findViewById<Button>(R.id.start_button)
-        startButton.setOnClickListener {
-            if (!running) {
-                setBaseTime()
-                stopwatch.start()
-                running = true
+        // Get a reference to the NavHostFragment and NavController
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavView.setupWithNavController(navController)
+
+        // Handle click on the bottom navigation item corresponding to the WelcomeFragment
+        bottomNavView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.welcomeFragment -> {
+                    // Navigate to WelcomeFragment
+                    if (navController.currentDestination?.id != R.id.welcomeFragment) {
+                        navController.navigate(R.id.welcomeFragment)
+                    }
+                    true // Indicate that the item click has been handled
+                }
+                R.id.timerFragment -> {
+                    // Navigate to timer fragment
+                    if (navController.currentDestination?.id != R.id.timerFragment) {
+                        navController.navigate(R.id.timerFragment)
+                    }
+                    true // Indicate that the item click has been handled
+                }
+                R.id.resultsFragment -> {
+                    // Navigate to Results fragment
+                    if (navController.currentDestination?.id != R.id.resultsFragment) {
+                        navController.navigate(R.id.resultsFragment)
+                    }
+                    true // Indicate that the item click has been handled
+                }
+                else -> false // Return false for other items
             }
         }
-
-        //pause button
-        val pauseButton = findViewById<Button>(R.id.pause_button)
-        pauseButton.setOnClickListener {
-            if (running) {
-                saveOffset()
-                stopwatch.stop()
-                running = false
-            }
-        }
-
-        //reset button
-        val resetButton = findViewById<Button>(R.id.reset_button)
-        resetButton.setOnClickListener {
-            offset = 0
-            setBaseTime()
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if (running) {
-            saveOffset()
-            stopwatch.stop()
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        if (running) {
-            setBaseTime()
-            stopwatch.start()
-            offset = 0
-        }
-    }
-
-    override fun onSaveInstanceState(savedInstanceState: Bundle) {
-        savedInstanceState.putLong(OFFSET_KEY, offset)
-        savedInstanceState.putBoolean(RUNNING_KEY, running)
-        savedInstanceState.putLong(BASE_KEY, stopwatch.base)
-        super.onSaveInstanceState(savedInstanceState)
-    }
-
-
-    fun setBaseTime() {
-        stopwatch.base = SystemClock.elapsedRealtime() - offset
-    }
-
-    fun saveOffset() {
-        offset = SystemClock.elapsedRealtime() - stopwatch.base
-    }*/
     }
 }
